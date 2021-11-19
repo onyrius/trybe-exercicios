e789ccd62e959f7f6f99e6ed8ba3b95243ac1aee
Suelen <suelenonyrius@gmail.com>
2021-11-19 11:38:36 -0300

    exercicios de fixacao 5.1

---
+++ Fundamentos/Bloco-05/dia-1-dom-e-seletores/script.js
@@ -0,0 +1,3 @@
+document.getElementById ("page-title").innerText= "Hello world";
+document.getElementById("second-paragraph").style.backgroundColor = "blue"
+document.getElementById("subtitle").innerText = "Falta pouco para viajar!!"
