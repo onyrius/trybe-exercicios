99e37b2bac4e34eea9ed5deea89db84986e402f4
Suelen <suelenonyrius@gmail.com>
2021-11-20 13:59:24 -0300

    exercicios de fixacao 5.1

---
+++ Fundamentos/Bloco-05/dia-1-dom-e-seletores/exercicio-5.1/script.js
@@ -0,0 +1,5 @@
+document.querySelector("#header-container").style.backgroundColor = "purple";
+document.getElementsByClassName("emergency-tasks")[0].style.backgroundColor = "red"
+document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor = "yellow"
+
+document.querySelector("#footer-container").style.backgroundColor = "black"
