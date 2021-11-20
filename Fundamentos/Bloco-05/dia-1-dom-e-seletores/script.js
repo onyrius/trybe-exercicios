e390e3d60ef9b81244bdd992411628c71ce71615
Suelen <suelenonyrius@gmail.com>
2021-11-20 14:01:04 -0300

    exercicios de fixacao 5.1

--- Fundamentos/Bloco-05/dia-1-dom-e-seletores/script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById ("page-title").innerText= "Hello world";
-document.getElementById("second-paragraph").style.backgroundColor = "blue"
-document.getElementById("subtitle").innerText = "Falta pouco para viajar!!"
---
+++ Fundamentos/Bloco-05/dia-1-dom-e-seletores/treino/treino.js
@@ -0,0 +1,24 @@
+document.getElementById ("page-title").innerText= "Hello world";
+document.getElementById("second-paragraph").style.backgroundColor = "blue";
+document.getElementById("subtitle").innerText = "Falta pouco para viajar!!"
+document.getElementById("paragraph").innerHTML 
+document.getElementsByClassName("test")[0,1];
+document.getElementsByClassName("test")[0].style.backgroundColor = "yellow";
+document.getElementsByTagName("h4")[0].style.color = "green";
+
+// document.querrySelector("#page-title")
+// //  "Hello world";
+// document.querySelector("second-paragraph").style.backgroundColor = "blue";
+// document.getElementById("subtitle").innerText = "Falta pouco para viajar!!"
+// document.getElementById("paragraph").innerHTML 
+// document.getElementsByClassName("test")[0,1];
+// document.getElementsByClassName("test")[0].style.backgroundColor = "yellow";
+// document.getElementsByTagName("h4")[0].style.color = "green";
+
+// document.querySelector("#page-title").innerText= "Hello world";
+// document.querySelector("#second-paragraph").style.backgroundColor = "blue";
+// document.querySelector("#subtitle").innerText = "Falta pouco para viajar!!"
+// document.querySelector("#paragraph").innerHTML 
+// document.querySelector(".test").style.backgroundColor = "yellow";
+// document.querySelector("header h4").style.color = "green";
+
