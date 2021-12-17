5be36076fe21fab952a0f3da045a89afa4795c21
Suelen Arruda <suelenonyrius@gmail.com>
2021-12-17 19:03:52 +0100

    exercicio 1

--- treinamento/script.js
+++ treinamento/script.js
@@ -1,8 +1,28 @@
-// Com o template literals
-console.log(`Primeira linha;
-Segunda linha;
-Terceira linha;`
-)
+/* function testingScope(escopo) {
+    if (escopo === true) {
+      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+      console.log(ifScope);
+    } else {
+      let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+    console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+  }
 
-// Sem o template literals:
-console.log('Primeira linha;\n' + 'Segunda linha;\n' + 'Terceira linha;\n')
+  testingScope(true); */
+
+  const testingScope = escopo => {
+      if (escopo) {
+        let ifScope = `Não devo ser utilizada fora do meu escopo (if)`
+        ifScope = `ótimo, fui utilizada no escopo !`
+        console.log(ifScope)
+      } else {
+        let elseScope = `Não devo ser utilizada fora meu escopo (else)`
+        console.log(elseScope);
+      }
+    }
+  
+    testingScope(true); 
+      
+  
