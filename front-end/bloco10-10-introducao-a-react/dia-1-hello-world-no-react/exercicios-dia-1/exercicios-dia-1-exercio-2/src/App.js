c96678b315506c0f3ecae5ace4458e30dc05ad62
Suelen Arruda <suelenonyrius@gmail.com>
2022-01-24 11:09:18 +0100

    reorganiza pastas dos exercicios 1 e 2 do primeiro dia React

---
+++ front-end/bloco10-10-introducao-a-react/dia-1-hello-world-no-react/exercicios-dia-1/exercicios-dia-1-exercio-2/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+
+import './App.css'
+
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+        <Header/>
+      </div>
+    )
+  }
+}
+
+export default App;
